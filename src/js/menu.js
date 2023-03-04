2c45ddcc1dedda5a1a69387badc0efe0ea74c767
Linda Pihl <linda.pihl@studerande.karlskogafhs.se>
2023-03-04 16:07:26 +0100

    Add js to menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,6 +2,7 @@ const menu = () => {
 
     const menu = document.querySelector('.site-menu')
     const menuButton = document.getElementById('btn')
+    const menuLinks = document.querySelectorAll('.site-menu__link')
     
     const toggleMenu = () => {
         menu.classList.toggle('site-menu--visible');
@@ -15,6 +16,13 @@ const menu = () => {
 
     menuButton.addEventListener('click', toggleMenu);   
    
+    const removeClassWhenClickOnLink = () => {
+        menu.classList.remove('site-menu--visible')
+    }
+
+    menuLinks.forEach(link => link.addEventListener('click', removeClassWhenClickOnLink))
+    
+    
 }
 
 menu()
